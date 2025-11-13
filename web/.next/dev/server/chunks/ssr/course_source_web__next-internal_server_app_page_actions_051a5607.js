e2c98ec4f4d884b2e29f6e2929c3c368d109e71b
Esteban <codigo47@gmail.com>
2025-11-13 11:51:19 -0300

    Remove .next folder from git tracking

--- web/.next/dev/server/chunks/ssr/course_source_web__next-internal_server_app_page_actions_051a5607.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/course/source/web/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=course_source_web__next-internal_server_app_page_actions_051a5607.js.map
